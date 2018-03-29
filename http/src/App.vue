65f112960b3f03b68368a633fb7fdcb299a48b08
Maciej Wasilak <wasilak@gmail.com>
2018-03-29 22:29:37 +0200

    Add http preparation

--- http/src/App.vue
+++ http/src/App.vue
@@ -2,7 +2,16 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Http</h1>
+                <h1>Https</h1>
+                <div class="form-group">
+                  <label>Username</label>
+                  <input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                  <label>Mail</label>
+                  <input type="text" class="form-control" v-model="user.mail">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
             </div>
         </div>
     </div>
@@ -10,6 +19,19 @@
 
 <script>
     export default {
+      data () {
+        return {
+          user: {
+            username: '',
+            mail: ''
+          }
+        }
+      },
+      methods : {
+        submit () {
+          console.log(this.user);
+        }
+      }
     }
 </script>
 
